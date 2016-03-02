da525ba6f62762b4947f2ba6b4f774d0dbbab061
Victor Ortiz <kespers@gmail.com>
2016-03-02 01:36:03 +0100

    New config.rb

--- config.rb
+++ config.rb
@@ -1,11 +1,3 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -14,33 +6,26 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+# page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
-  activate :bourbon
-  set :debug_assets, true
 end
+
+###
+# Helpers
+###
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -49,28 +34,11 @@ end
 #   end
 # end
 
-set :css_dir, 'assets/stylesheets'
-
-set :js_dir, 'assets/javascripts'
-
-set :images_dir, 'assets/images'
-
-set :fonts_dir, 'assets/fonts'
-
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
+  # Minify CSS on build
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # activate :minify_javascript
 end
